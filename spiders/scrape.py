6acea2e4508820a5f66295588665cadacf27d456
skiidk <sidheshdesaidev@gmail.com>
2020-07-22 23:20:56 -0400

    Switched spider to look at all files

--- spiders/scrape.py
+++ spiders/scrape.py
@@ -10,52 +10,52 @@ class BrickSetSpider(scrapy.Spider):
     base_url = "https://www.us-cert.gov"
     page_url = "https://www.us-cert.gov/ncas/analysis-reports"
 
-    def parse(self, response):
-        print(response.url)
-        # debugging
-
-        selectors = response.xpath("//a")
-        # Get all the <a>'s and loop through them
-        for selector in selectors:
-            link = selector.xpath("@href").extract_first()
-            text = selector.xpath("text()").extract_first()
-            # Getting the link and link text from the tags
-
-            if link is not None and text is not None and text.startswith("MIFR"):
-                # For the links corresponding with MIFR, run the parse_page method
-                print("link")
-                print(link)
-                print("text")
-                print(text)
-                yield scrapy.Request(self.base_url + link, callback=self.parse_page)
-
-        next_page_partial_url = response.xpath(
-            '//li[@class="pager__item pager__item--next"]/a/@href'
-        ).extract_first()
-        # Get the next page url from the next button's link
-        if next_page_partial_url is not None:
-            next_page_url = self.page_url + next_page_partial_url
-            yield scrapy.Request(next_page_url, callback=self.parse)
-            # Rerun the method on the next page
-        else:
-            print("done")
-            yield None
-
-    ##This is the archived, link-based parse method. It is more comprehensive since it selects all of the relevant URLs rather than
-    ##going by the name (since a lot of the files don't start with MIFR)
     # def parse(self, response):
+    #     print(response.url)
+    #     # debugging
+
     #     selectors = response.xpath("//a")
+    #     # Get all the <a>'s and loop through them
     #     for selector in selectors:
     #         link = selector.xpath("@href").extract_first()
-    #         exts = ['ar20', 'ar19', 'AR19', 'AR18']
-    #         if link is not None and any(ext in link for ext in exts):
-    #             yield scrapy.Request(self.base_url+link, callback=self.parse_page)
-    #     next_page_partial_url = response.xpath('//li[@class="pager__item pager__item--next"]/a/@href').extract_first()
+    #         text = selector.xpath("text()").extract_first()
+    #         # Getting the link and link text from the tags
+
+    #         if link is not None and text is not None and text.startswith("MIFR"):
+    #             # For the links corresponding with MIFR, run the parse_page method
+    #             print("link")
+    #             print(link)
+    #             print("text")
+    #             print(text)
+    #             yield scrapy.Request(self.base_url + link, callback=self.parse_page)
+
+    #     next_page_partial_url = response.xpath(
+    #         '//li[@class="pager__item pager__item--next"]/a/@href'
+    #     ).extract_first()
+    #     # Get the next page url from the next button's link
     #     if next_page_partial_url is not None:
     #         next_page_url = self.page_url + next_page_partial_url
     #         yield scrapy.Request(next_page_url, callback=self.parse)
+    #         # Rerun the method on the next page
     #     else:
+    #         print("done")
     #         yield None
+
+    ##This is the archived, link-based parse method. It is more comprehensive since it selects all of the relevant URLs rather than
+    ##going by the name (since a lot of the files don't start with MIFR)
+    def parse(self, response):
+        selectors = response.xpath("//a")
+        for selector in selectors:
+            link = selector.xpath("@href").extract_first()
+            exts = ['ar20', 'ar19', 'AR19', 'AR18']
+            if link is not None and any(ext in link for ext in exts):
+                yield scrapy.Request(self.base_url+link, callback=self.parse_page)
+        next_page_partial_url = response.xpath('//li[@class="pager__item pager__item--next"]/a/@href').extract_first()
+        if next_page_partial_url is not None:
+            next_page_url = self.page_url + next_page_partial_url
+            yield scrapy.Request(next_page_url, callback=self.parse)
+        else:
+            yield None
 
     def parse_page(self, response):
 
